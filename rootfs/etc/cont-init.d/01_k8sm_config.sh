ac6ec950835d4d1e72ccab7010b9e797a2d2c397
Alexander Litvinenko <altvnk@me.com>
2015-06-04 01:54:38 +0300

    Update runtime config generation script

--- rootfs/etc/cont-init.d/01_k8sm_config.sh
+++ rootfs/etc/cont-init.d/01_k8sm_config.sh
@@ -1,18 +1,8 @@
 #!/usr/bin/with-contenv sh
 # This script composes mesos-cloud.conf from env vars.
-
-# cat > ${K8SM_HOME}/mesos-cloud.conf <<EOL
-# [mesos-cloud]
-#     mesos-master        = ${MESOS_HOST}:${MESOS_PORT}
-#     http-client-timeout = 5s
-#     state-cache-ttl     = 20s
-# EOL
-
-K8SM_CONFIG="${K8SM_HOME}/mesos-cloud.conf"
-if [ ! -f "$K8SM_CONFIG" ]; then
-    echo "[mesos-cloud]
-        mesos-master        = ${MESOS_HOST}:${MESOS_PORT}
-        http-client-timeout = 5s
-        state-cache-ttl     = 20s
-    " > ${K8SM_CONFIG}
-fi
+cat > ${K8SM_HOME}/mesos-cloud.conf <<EOL
+[mesos-cloud]
+    mesos-master        = ${MESOS_HOST}:${MESOS_PORT}
+    http-client-timeout = 5s
+    state-cache-ttl     = 20s
+EOL

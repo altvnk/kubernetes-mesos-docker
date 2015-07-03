c625e3863b572f0726aa2c177828bf9ce7042450
Alexander Litvinenko <altvnk@edi.su>
2015-07-03 16:47:19 +0300

    Fix errors

--- rootfs/etc/cont-init.d/01_k8sm_config.sh
+++ rootfs/etc/cont-init.d/01_k8sm_config.sh
@@ -2,7 +2,7 @@
 # This script composes mesos-cloud.conf from env vars.
 cat > ${K8SM_HOME}/mesos-cloud.conf <<EOL
 [mesos-cloud]
-    mesos-master        = ${MESOS_HOST}:${MESOS_PORT}
+    mesos-master        = ${MESOS_HOST:-leader.mesos.service.consul}:${MESOS_PORT:-5050}
     http-client-timeout = 5s
     state-cache-ttl     = 20s
 EOL
